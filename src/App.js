9c5d62e7191d363565c813c8fd15e0971737d4f5
dong5397 <dongwok5397@naver.com>
2024-11-29 13:43:16 +0900

    노션 완료

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Editor from './components/editor/Editor.js'
+import EditorPage from './components/editor/EditorPage.js'
+import SidebarPage from './components/sidebar/SidebarPage.js'
+
+export default function App({ $target }) {
+    const editorDummyData = {
+        parent: 'new',
+        title: '노션을 만들자',
+        content: '노션 만들기~',
+    }
+
+    const $listContainer = document.createElement('div')
+    const $editorContainer = document.createElement('div')
+
+    $target.appendChild($listContainer)
+    $target.appendChild($editorContainer)
+
+    const initalState = []
+
+    const editorPage = new EditorPage({
+        $target: $editorContainer,
+        initialState: editorDummyData,
+    })
+    const onEditing = (id) => {
+        console.log(id)
+    }
+    const sidebarPage = new SidebarPage({
+        $target: $listContainer,
+        initalState,
+        onEditing,
+    })
+
+    sidebarPage.setState()
+}
